6537d946860017c75ce97214ffd417d8d00b1681
Tarun Mittal <tm6622@g.rit.edu>
2023-05-15 22:59:44 -0400

    cropped out char

--- images.py
+++ images.py
@@ -2,4 +2,6 @@ from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.convert('L')
-filtered_img.save("grey.png", 'png')
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("grey.png", 'png')
