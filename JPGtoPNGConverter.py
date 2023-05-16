6a8272d848e569ddd0d2c496c7c1900075fc22d9
Tarun Mittal <tm6622@g.rit.edu>
2023-05-15 23:08:09 -0400

    added structure to JPGtoPNGconv

---
+++ JPGtoPNGConverter.py
@@ -0,0 +1,17 @@
+import sys
+import os 
+from PIL import Image
+
+# grab first and second argument 
+
+
+
+# check if new exists, if not then create it
+
+
+
+
+# loop through Pokedex, convert images to png 
+
+
+# save to the new folder 
