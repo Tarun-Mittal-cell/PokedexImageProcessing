692f22be06b4abda4ef41181c3ee916d71ae76ad
Tarun Mittal <tm6622@g.rit.edu>
2023-05-15 23:03:51 -0400

    added profile picture

--- images.py
+++ images.py
@@ -1,7 +1,5 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.convert('L')
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
-region.save("grey.png", 'png')
+img = Image.open('./astro.jpg')
+img.thumbnail((400, 400))
+img.save('thumbnail.jpg')
